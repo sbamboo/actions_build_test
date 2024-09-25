5924a234e0dd4f9f3658a7996e87c284e105c3f0
StalkedBamboo <85576459+sbamboo@users.noreply.github.com>
2024-09-25 22:45:28 +0200

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -8,16 +8,20 @@ import sys,os
 if getattr(sys, 'frozen', False):
     application_path = os.path.dirname(sys.executable)
     running_mode = 'Frozen/executable'
+    executable_path = sys.executable
 else:
     try:
         app_full_path = os.path.realpath(__file__)
         application_path = os.path.dirname(app_full_path)
         running_mode = "Non-interactive (e.g. 'python myapp.py')"
+        executable_path = sys.executable
     except NameError:
         application_path = os.getcwd()
         running_mode = 'Interactive'
+        executable_path = sys.executable
 
 
 
 console.print('Running mode:', running_mode, style="magenta")
-console.print('  Appliction path  :', application_path, style="magenta")
+console.print('  Application path  :', application_path, style="magenta")
+console.print('  Executable path   :', executable_path, style="magenta")
