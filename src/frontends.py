064175974dc8b478f3bc4d93b22e6dfed20f74b1
StalkedBamboo <85576459+sbamboo@users.noreply.github.com>
2024-09-27 22:51:13 +0200

    Update frontends.py

--- src/frontends.py
+++ src/frontends.py
@@ -1,1 +1,4 @@
 # Define the main frontend-engine aswell as the class that frontends should inherit from
+
+# main/start/launch -> Launches "second-window" applications, and starts listerners if needed
+# eof/stop/terminate -> Closes "second-window" applications, and stops eventual listeners
